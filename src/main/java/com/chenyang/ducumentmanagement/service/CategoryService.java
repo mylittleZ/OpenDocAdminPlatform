cf86fad14d7184e5873a1b5e735802647ebef201
mylittleZ <1020464190@qq.com>
2024-01-18 16:56:30 -0800

    feat: add new category feature to document management

---
+++ src/main/java/com/chenyang/ducumentmanagement/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.chenyang.ducumentmanagement.service;
+
+import com.chenyang.ducumentmanagement.pojo.Category;
+
+public interface CategoryService {
+    void add(Category category);
+}
---
+++ src/main/java/com/chenyang/ducumentmanagement/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,27 @@
+package com.chenyang.ducumentmanagement.service.impl;
+
+
+import com.chenyang.ducumentmanagement.mapper.CategoryMapper;
+import com.chenyang.ducumentmanagement.pojo.Category;
+import com.chenyang.ducumentmanagement.service.CategoryService;
+import com.chenyang.ducumentmanagement.utils.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Override
+    public void add(Category category) {
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        Map<String,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        category.setCreateUser(userId);
+        categoryMapper.add(category);
+    }
+}
